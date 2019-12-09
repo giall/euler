3235286b5f5acff84cd52376bb544752a1473d55
Christos Giallouros <10516560+christos-giallouros@users.noreply.github.com>
2019-12-09 21:10:58 +0000

    requirements.txt and markdown

--- src/helpers.py
+++ src/helpers.py
@@ -1,11 +1,18 @@
 
 import logging
+from textwrap import dedent
 from time import time
+from markdown import markdown
 from flask import render_template, g, url_for
 
 
+def format_description(description):
+    dedented = dedent(description)
+    return markdown(dedented)
+
+
 def render_solution(solution, title, input=None):
-    description = solution.__doc__
+    description = format_description(solution.__doc__)
     if (input == None):
         input = solution.__defaults__[0]
     try:
@@ -18,19 +25,25 @@ def render_solution(solution, title, input=None):
         return render_template('error.html')
 
 
+def get_problem(rule, func):
+    endpoint = rule.endpoint
+    problem = {}
+    problem['name'] = func.__doc__ or 'Untitled'
+    problem['number'] = endpoint.split('_')[1]
+    if (len(rule.arguments) == 0):
+        problem['url'] = url_for(endpoint)
+    else:
+        problem['url'] = url_for(endpoint, input=4)
+    return problem
+
+
 def problems_list(url_map, globals):
     problems = []
     for rule in url_map.iter_rules():
         endpoint = rule.endpoint
         if ('problem_' not in endpoint):
             continue
-        problem = {}
         func = globals.get(endpoint)
-        problem['name'] = func.__doc__
-        problem['number'] = endpoint.split('_')[1]
-        if (len(rule.arguments) == 0):
-            problem['endpoint'] = url_for(endpoint)
-        else:
-            problem['endpoint'] = url_for(endpoint, input=4)
+        problem = get_problem(rule, func)
         problems.append(problem)
     return problems
